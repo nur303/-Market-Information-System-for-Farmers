b59ccfa358870464056d0521d51f7190964eebac
Pitak M <131612585+Pitak-Chakma@users.noreply.github.com>
2024-12-07 16:22:04 +0600

    pitak push

---
+++ src/main/java/com/example/demo2/farmerconnect/FarmerController.java
@@ -0,0 +1,25 @@
+package com.example.demo2.farmerconnect;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.List;
+@Controller
+public class FarmerController {
+    @Autowired
+     TargetUserService targetUserService;
+
+    @ModelAttribute("userDataList")
+    public List<TargetUser_T> userNames() {
+        return targetUserService.getAllTheUsers();
+    }
+    @GetMapping("/farmer")
+    public String getAllFarmers() {
+        return "Farmer"; // This is the name of the HTML page that will display the data
+    }
+
+
+
+}
---
+++ src/main/java/com/example/demo2/farmerconnect/TargetUser_T.java
@@ -0,0 +1,46 @@
+package com.example.demo2.farmerconnect;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+@Data
+@Entity
+@Table(
+        name = "TARGET_USER_T"
+)
+public class TargetUser_T {
+
+        @Id
+        private String userID;
+        private String userName;
+        private String areaCode ;
+        private int phoneNumber;
+        private Date dateOfRegistration;
+        private String userType;
+
+        public TargetUser_T(String area_code, int phone_number, String userID, String userName, String userType) {
+            this.areaCode = area_code;
+            this.phoneNumber = phone_number;
+            this.userID = userID;
+            this.userName = userName;
+            this.userType = userType;
+        }
+
+        public TargetUser_T(int phone_number, String area_code, String userName, String userType) {
+
+            this.phoneNumber = phone_number;
+            this.areaCode = area_code;
+            this.userName = userName;
+            this.userType = userType;
+        }
+
+        public TargetUser_T() {
+        }
+
+
+}
+
+
