3fa27f684f91232808ad7975ce1e7132c096c61b
himelpro <129043272+himel7009@users.noreply.github.com>
2024-12-08 19:53:29 +0600

    backend_parts

---
+++ crops_for_bidding/src/test/java/com/example/crops_for_bidding/CropsForBiddingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crops_for_bidding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropsForBiddingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
