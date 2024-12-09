71d5c7bec07ea242efaacfcbccf866ac76cc3576
himelpro <129043272+himel7009@users.noreply.github.com>
2024-12-10 00:11:05 +0600

    commit_don't know
    deleted after the decison

--- crops_for_bidding/src/main/java/com/example/crops_for_bidding/repositories/buyerrepositories.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Buyer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BuyerRepository extends JpaRepository<Buyer, Long> {
-}
--- crops_for_bidding/src/main/java/com/example/crops_for_bidding/repositories/croprepositories.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Crop;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CropRepository extends JpaRepository<Crop, Long> {
-}
