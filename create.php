66de733d64352b06f87509645d28e2a2da0c1a33
Pitak <131612585+Pitak-Chakma@users.noreply.github.com>
2024-12-14 23:41:45 +0600

    Add files via upload

--- create.php
+++ create.php
@@ -1,80 +1,46 @@
-<?php
-    include "connection.php";
-    
-    if(isset($_POST['submit'])){
-        // Validate input
-        if(!empty($_POST['crop_id']) && !empty($_POST['farmer_id']) && !empty($_POST['quantity']) && !empty($_POST['bid_end_time'])) {
-            // Use prepared statement to prevent SQL injection
-            $stmt = $conn->prepare("INSERT INTO crud1 (crop_id, farmer_id, quantity, bid_end_time) VALUES (?, ?, ?, ?)");
-            $stmt->bind_param("ssis", $_POST['crop_id'], $_POST['farmer_id'], $_POST['quantity'], $_POST['bid_end_time']);
-            
-            if($stmt->execute()) {
-                header("Location: index.php");
-                exit;
-            } else {
-                echo "Error: " . $stmt->error;
-            }
-            $stmt->close();
-        } else {
-            echo "<div class='alert alert-danger'>All fields are required!</div>";
-        }
-    }
-?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>CRUD - Create</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <title>Add New Book</title>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="index.php">PHP CRUD OPERATION</a>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="create.php"><span style="font-size:larger;">Add New</span></a>
-                    </li>
-                </ul>
+    <div class="container my-5">
+    <header class="d-flex justify-content-between my-4">
+            <h1>Add New Book</h1>
+            <div>
+            <a href="index.php" class="btn btn-primary">Back</a>
             </div>
-        </div>
-    </nav>
-    <div class="col-lg-6 m-auto">
-        <form method="post">
-            <br><br>
-            <div class="card">
-                <div class="card-header bg-primary">
-                    <h1 class="text-white text-center">Create New Bid</h1>
-                </div>
-                <div class="card-body">
-                    <div class="form-group">
-                        <label>Crop ID:</label>
-                        <input type="text" name="crop_id" class="form-control" required>
-                    </div>
-                    <div class="form-group">
-                        <label>Farmer ID:</label>
-                        <input type="text" name="farmer_id" class="form-control" required>
-                    </div>
-                    <div class="form-group">
-                        <label>Quantity:</label>
-                        <input type="number" name="quantity" class="form-control" required>
-                    </div>
-                    <div class="form-group">
-                        <label>Bid End Time:</label>
-                        <input type="datetime-local" name="bid_end_time" class="form-control" required>
-                    </div>
-                    <button class="btn btn-success" type="submit" name="submit">Submit</button>
-                    <a class="btn btn-info" href="index.php">Cancel</a>
-                </div>
+        </header>
+        
+        <form action="process.php" method="post">
+            <div class="form-elemnt my-4">
+                <input type="text" class="form-control" name="title" placeholder="Book Title:">
+            </div>
+            <div class="form-elemnt my-4">
+                <input type="text" class="form-control" name="author" placeholder="Author Name:">
+            </div>
+            <div class="form-elemnt my-4">
+                <select name="type" id="" class="form-control">
+                    <option value="">Select Book Type:</option>
+                    <option value="Adventure">Adventure</option>
+                    <option value="Crime">Crime</option>
+                    <option value="Fantasy">Fantasy</option>
+                    <option value="Horror">Horror</option>
+                </select>
+            </div>
+            <div class="form-element my-4">
+                <textarea name="description" id="" class="form-control" placeholder="Book Description:"></textarea>
+            </div>
+            <div class="form-element my-4">
+                <input type="submit" name="create" value="Add Book" class="btn btn-primary">
             </div>
         </form>
+        
+        
     </div>
 </body>
 </html>
